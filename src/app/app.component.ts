a8373ac9ce99c8f2e6d62087c70c50fa7d882f34
eowagney <wagnergomes9a@gmail.com>
2025-08-30 21:52:15 -0300

    site editado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,54 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormGroup, FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MatMenuModule, MatMenuPanel } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatStepperModule } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    RouterOutlet,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatStepperModule
+],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   pessoas: any[] = [];
   novaPessoa: any = {};
+  
+Menu: MatMenuPanel<any> | null | undefined;
+formGroup: FormGroup<any> | undefined;
 
   constructor(private http: HttpClient) {
     this.listarPessoas();
@@ -41,3 +78,4 @@ export class AppComponent {
       });
   }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
+
+
+
 
 @NgModule({ 
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule, 
+    FormsModule, 
+    HttpClientModule,
+  ],
+
+
 })
 export class AppModule {}
